28c53010ecee305970440d0639b9beec3b87bb73
DJ Hubka <mrfatgoat@gmail.com>
2017-08-29 21:46:00 -0700

    Delete XML file to recycle bin when done processing.

--- VsDiffDuplicateHandler/Program.cs
+++ VsDiffDuplicateHandler/Program.cs
@@ -48,6 +48,8 @@ namespace VsDiffDuplicateHandler
 
             ProcessDupeFile(xdoc);
 
+            FileSystem.DeleteFile(xmlPath, UIOption.OnlyErrorDialogs, RecycleOption.SendToRecycleBin);
+
             Console.ReadLine();
             return;
         }
